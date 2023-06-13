5ea0758c30428740a59b991b450f21ddc45fe071
Josh T <114734188+IkuStudies@users.noreply.github.com>
2023-06-13 10:24:12 -0500

    Create gamelogic.py

---
+++ BRAIN/gamelogic.py
@@ -0,0 +1,16 @@
+class King
+    def move #move one square up right left down or diaganol
+        if not upgraded
+        if upgraded #move one square plus special
+class Queen
+    def move 
+        if not upgraded #regular queen moves
+        if upgraded #plus special
+class Jack_Knight
+class Ace_Rook
+class Nine_Bishop
+class Pawn
+class Joker
+
+def islegal
+    def check #for possible legal moves, en passant, castling
